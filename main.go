3766d0ca53ccf0112549aa1109426c42cbb2af79
Rauru <rahulbista850@gmail.com>
2024-06-30 19:34:38 +0545

    simple go webserver

--- main.go
+++ main.go
@@ -1,17 +1,42 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	"net/http"
 )
+
+func handleForm(w http.ResponseWriter, r *http.Request){
+	if err:= r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "parseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful!\n")
+	name := r.FormValue("name")
+	age := r.FormValue("age")
+	fmt.Fprintf(w, "Name = %v \nAge = %v", name, age)
+}
+
+func handleHello(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET"{
+		http.Error(w, "Method not found", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello")
+}
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
+	http.HandleFunc("/form", handleForm)
+	http.HandleFunc("/hello", handleHello)
 
-	err := http.ListenAndServe(":8080", nil)
 
-	if err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic("error")
 	}
 }
